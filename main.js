95075844793eddbc721bfaa3447a3ea5d60a8c17
hjoyheath <hillaryjoyheath@gmail.com>
2022-04-02 15:01:07 -0400

    resolved overflow

--- main.js
+++ main.js
@@ -1,43 +1,39 @@
 const result = document.querySelector(".result");
-console.log('show display', result);
+console.log("show display", result);
 
-const minus = document.getElementById('minus');
+const minus = document.getElementById("minus");
 console.log("show minus", minus);
-const plus = document.getElementById('plus');
+const plus = document.getElementById("plus");
 console.log("show plus", plus);
-const divide = document.getElementById('divide');
+const divide = document.getElementById("divide");
 console.log("show divide", divide);
-const multiply = document.getElementById('multiply');
+const multiply = document.getElementById("multiply");
 console.log("show multiply", multiply);
-const equals = document.getElementById('equals');
+const equals = document.getElementById("equals");
 console.log("show equals", equals);
-const clear = document.getElementById('clear');
+const clear = document.getElementById("clear");
 console.log("show clear", clear);
-const decimal = document.getElementById('decimal');
+const decimal = document.getElementById("decimal");
 console.log("show decimal", decimal);
 
-let buttons = document.querySelectorAll('.button')
-    for (let button of buttons){
-        button.addEventListener('click', function (event){
-            console.log(event.target.innerText)
-    
+let buttons = document.querySelectorAll(".button");
+for (let button of buttons) {
+  button.addEventListener("click", function (event) {
+    console.log(event, event.target.innerText);
+
     if (event.target.id !== "result") {
-        result.innerText += event.target.id;
-        console.log("event object", event.target.id);
+      result.innerText += event.target.id;
+      console.log("event object", event.target.id);
     }
 
-    if (event.target.id ==="clear"){
-        result.innerText = "";
-        console.log("event object", event.target.id);
-
+    if (event.target.id === "clear") {
+      result.innerText = "";
+      console.log("event object", event.target.id);
     }
 
-    if (event.target.id === "result"){
-        let result = result(result.innerText)
-        result.innerText = result
-        
+    if (event.target.id === "result") {
+      let result = result(result.innerText);
+      result.innerText = result;
     }
-            
-
-        });
+  });
 }
